6dcecaeeb61316d445987511cb6c0ea06c4656d7
Till Beemelmanns <till.beemelmanns@gmail.com>
2026-06-19 15:46:19 +0200

    show file size as humanreadable

--- config/yazi/init.lua
+++ config/yazi/init.lua
@@ -1,12 +1,17 @@
 function Linemode:mtime_ls()
 	local mtime = self._file.cha.mtime
-	if not mtime then
-		return ""
+	local date = ""
+	if mtime then
+		local t = math.floor(mtime)
+		if os.date("%Y", t) == os.date("%Y") then
+			date = os.date("%b %e %H:%M", t)
+		else
+			date = os.date("%b %e  %Y", t)
+		end
 	end
-	local t = math.floor(mtime)
-	if os.date("%Y", t) == os.date("%Y") then
-		return os.date("%b %e %H:%M", t)
-	else
-		return os.date("%b %e  %Y", t)
-	end
+
+	local size = self._file:size()
+	local size_str = size and ya.readable_size(size) or "-"
+
+	return string.format("%6s  %s", size_str, date)
 end
